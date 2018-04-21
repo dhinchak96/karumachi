005754770a1b82579de38266fe1f15f5f6f4ee92
dhinchak96 <nekramprajapatics@gmail.com>
2018-04-20 21:07:19 -0700

    Hashing

---
+++ hashing12.cpp
@@ -0,0 +1,32 @@
+//hashing problem 
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int m,n;
+	cin>>m>>n;
+				int a[m],b[n],i,j,k;
+	for(i=0;i<m;i++)	
+			cin>>a[i];	
+	for(j=0;j<n;j++)
+			cin>>b[j];
+	cin>>k;
+		if(m<n)
+		{
+			for(i=0;i<m;i++)
+				hash[a[i]]++;
+		for(i=0;i<100;i++)
+			{
+				for(j=0;j<n;j++)
+						
+			}                   // unsolved 
+		}
+		else{
+			for(i=0;i<n;i++)
+				hash[b[i]]++;
+		}
+		
+return 0;
+	 
+}
